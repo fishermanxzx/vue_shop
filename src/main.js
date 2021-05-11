868b11fdd8a682b51a1e9b837b836eeed1152197
fishermanxzx <1015776502@qq.com>
2021-05-12 01:01:19 +0800

    新增了登陆后基础页面

--- src/main.js
+++ src/main.js
@@ -8,6 +8,11 @@ import axios from 'axios'
 
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // 添加请求头
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
